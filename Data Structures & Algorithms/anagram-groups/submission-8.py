445a41666f585fcb7e48462ed718db77397ea30f
Sameer Sawant <its4.collegeiitd@gmail.com>
2026-07-07 18:24:20 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def sorting(s):
+            return ''.join(sorted(s))
+
+        sorted_strs = []
+        for x in strs:
+            sorted_strs.append(sorting(x))
+
+        seen = {}
+        for i in range(len(strs)):
+            if sorted_strs[i] in seen:
+                seen[sorted_strs[i]].append(strs[i])
+
+            else:
+                seen[sorted_strs[i]] = [strs[i]]
+
+        return list(seen.values())
+        
+            
